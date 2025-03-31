f3e1ae9bc795f4236d2e979c33854c1b137c40bd
Valentin Lefort <lefortvalentin94@gmail.com>
2025-03-31 13:43:25 +0200

    add the banners

--- components/AdBanner.js
+++ components/AdBanner.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 
-const AdBanner = () => {
+const AdBanner = ({ adSlot, format = "auto", layout = "", style = {} }) => {
   useEffect(() => {
     try {
       (window.adsbygoogle = window.adsbygoogle || []).push({});
@@ -10,16 +10,15 @@ const AdBanner = () => {
   }, []);
 
   return (
-    <div className="my-4">
-      <ins
-        className="adsbygoogle"
-        style={{ display: "block" }}
-        data-ad-client={process.env.NEXT_PUBLIC_ADSENSE_CLIENT}
-        data-ad-slot={process.env.NEXT_PUBLIC_ADSENSE_SLOT}
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      ></ins>
-    </div>
+    <ins
+      className="adsbygoogle"
+      style={{ display: "block", ...style }}
+      data-ad-client={process.env.NEXT_PUBLIC_ADSENSE_CLIENT}
+      data-ad-slot={adSlot}
+      data-ad-format={format}
+      data-ad-layout={layout}
+      data-full-width-responsive="true"
+    ></ins>
   );
 };
 
