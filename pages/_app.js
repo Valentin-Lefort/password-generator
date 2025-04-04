4edb066a443e7ab40d1c8b7d1003378615cb9474
Valentin Lefort <lefortvalentin94@gmail.com>
2025-04-04 12:10:22 +0200

    add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,13 @@ import { Analytics } from "@vercel/analytics/react"
 <link rel="preload" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" as="style" />
 
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-  
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
+
+export default MyApp;
