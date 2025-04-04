aae68ee7724f0138cc6fd9143a731af9d0043843
Valentin Lefort <lefortvalentin94@gmail.com>
2025-04-04 12:04:08 +0200

    add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 <link rel="preload" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" as="style" />
 
